db5442bba640080ade199a886dbeca9d255c165c
jiq4001 <jingjingqi@gmail.com>
2020-02-08 11:41:48 -0500

    1

--- wk5/FastqcPool/R/idx_gen.R
+++
@@ -1,9 +0,0 @@
-idx_gen <- function(dataframe, sep = "_", col.names = c("Sample", "Biorep", "Techrep")){
-  key.df <- tidyr::separate(dataframe, sample_id, into = col.names, sep = sep)
-  if(sum(stats::complete.cases(key.df)) != nrow(dataframe)){
-    stop("check error in rename vector")
-  }
-  else{
-    return(key.df)
-  }
-}
